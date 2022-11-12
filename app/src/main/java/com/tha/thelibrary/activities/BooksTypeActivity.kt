98de05db06ccdeb0ce01fe87569ecf27927e869b
THA <thanhtaikaung90@gmail.com>
2022-11-12 11:45:03 +0630

    Add Header and set Recycler view with GridlayoutManager in BooksTypeActivity

--- app/src/main/java/com/tha/thelibrary/activities/BooksTypeActivity.kt
+++ app/src/main/java/com/tha/thelibrary/activities/BooksTypeActivity.kt
@@ -4,9 +4,16 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
 import com.tha.thelibrary.R
+import com.tha.thelibrary.adapters.CarouselAdapter
+import com.tha.thelibrary.adapters.ChildRecyclerAdapter
+import kotlinx.android.synthetic.main.activity_books_type.*
 
 class BooksTypeActivity : AppCompatActivity() {
+
+    private lateinit var mChildRecyclerAdapter: ChildRecyclerAdapter
 
     companion object{
         fun newIntent(context: Context): Intent {
@@ -17,5 +24,13 @@ class BooksTypeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books_type)
+        setUpBooksTypeRecycler()
+    }
+
+    //setup BooksType Recycler View
+    private fun setUpBooksTypeRecycler(){
+        mChildRecyclerAdapter = ChildRecyclerAdapter()
+        rvBooksType.adapter = mChildRecyclerAdapter
+        rvBooksType.layoutManager = GridLayoutManager(parent,2,GridLayoutManager.VERTICAL,false)
     }
 }
